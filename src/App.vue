ab2b5e536aadfb33e7aa0cbda0fb5dc8bd8971ad
Ian McPhail <webguyian@gmail.com>
2022-10-24 12:40:25 -0400

    Update HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,42 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="Hello Vue!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="root">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss">
+#app,
+.root {
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.root {
+  scrollbar-width: thin;
+  scrollbar-color: $black-dark rgba($black, 0.1);
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  &::-webkit-scrollbar {
+    max-width: $border-radius-m;
+    max-height: $border-radius-m;
+    background-color: transparent;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  &::-webkit-scrollbar-thumb {
+    border-radius: $border-radius-m;
+    background-color: $black-dark;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  &::-webkit-scrollbar-track {
+    border-radius: $border-radius-m;
+    box-shadow: inset 0 4px 12px rgba($black, 0.15);
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  &::-webkit-scrollbar-button,
+  &::-webkit-scrollbar-corner {
+    display: none;
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
+<template>
+  <div class="iportfolio-app">
+    <DeviceFrame></DeviceFrame>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
+import DeviceFrame from '@/components/DeviceFrame.vue';
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss">
+.iportfolio-app {
+  position: relative;
+  height: 100%;
+  text-align: center;
+
+  @include responsive($width-medium) {
+    height: auto;
+    margin-top: 5vh;
+  }
+
+  &-view {
+    height: 100%;
+    padding-top: $gutter-m;
+
+    &--no-padding {
+      padding-top: 0;
+    }
+  }
+}
+</style>
