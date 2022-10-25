632540aa14b38e1a5aa9c6e58a8dd09d766c346a
Ian McPhail <webguyian@gmail.com>
2022-10-25 13:46:25 -0400

    Create ClockFace and ClockHand components

---
+++ src/components/ClockHand.vue
@@ -0,0 +1,31 @@
+<template>
+  <line
+    :class="className"
+    :x1="x1"
+    :x2="x2"
+    :y1="y1"
+    :y2="length"
+    :transform="`rotate(${angle} ${width} ${width})`"
+  />
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    angle: number;
+    className: string;
+    x1?: number;
+    x2?: number;
+    y1?: number;
+    length?: number;
+    width?: number;
+  }>(),
+  {
+    x1: 30,
+    x2: 30,
+    y1: 30,
+    length: 60,
+    width: 30
+  }
+);
+</script>
