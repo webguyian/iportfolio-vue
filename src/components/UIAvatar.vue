3a8dc4da22d00ee3cc1b0c89faa8b87e3c7e3068
Ian McPhail <webguyian@gmail.com>
2022-10-24 22:57:18 -0400

    Create UIAvatar component

---
+++ src/components/UIAvatar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="ui-avatar">
+    <img v-if="src" class="ui-avatar-image" :alt="alt" :src="src" />
+    <UIText v-if="text" class="ui-avatar-text" modifier="light">{{
+      text
+    }}</UIText>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UIText from './UIText.vue';
+
+defineProps<{
+  alt?: string;
+  src?: string;
+  text?: string;
+}>();
+</script>
+
+<style lang="scss">
+.ui-avatar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 150px;
+  height: 150px;
+  border-radius: $border-radius-round;
+  background-color: $gray-medium;
+  text-align: center;
+
+  &-image {
+    border-radius: $border-radius-round;
+  }
+
+  &-text {
+    margin: 0;
+    font-size: 60px;
+  }
+}
+</style>
