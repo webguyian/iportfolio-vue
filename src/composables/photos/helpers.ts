86259005b95a8afcd07cfddb8c01d6149a5f428e
Ian McPhail <webguyian@gmail.com>
2022-11-14 22:37:22 -0500

    Create photos helpers

---
+++ src/composables/photos/helpers.ts
@@ -0,0 +1,30 @@
+import {
+  format,
+  formatDistanceToNowStrict,
+  differenceInCalendarWeeks,
+  toDate
+} from 'date-fns';
+import type { Photo } from '@/composables/camera/types';
+
+export const formatDate = (date: Date | number) =>
+  format(date, `MMM d, yyyy 'at' h:mm a`);
+
+export const getCreatedDate = (photo: Photo) => {
+  return photo.metadata.dateCreated;
+};
+
+export const formatCreatedDate = (photo: Photo) => {
+  return formatDate(getCreatedDate(photo));
+};
+
+export const formatCreatedDateRelative = (photo: Photo) => {
+  const date = toDate(getCreatedDate(photo));
+  const options = { addSuffix: true };
+  const differentWeek = differenceInCalendarWeeks(new Date(), date);
+
+  if (differentWeek < 2) {
+    return formatDistanceToNowStrict(date, options);
+  }
+
+  return formatDate(date);
+};
