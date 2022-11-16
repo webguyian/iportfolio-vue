0670593279d350c8b9a45ac4033726d983ac7d8f
Ian McPhail <webguyian@gmail.com>
2022-11-16 10:48:55 -0500

    Create MusicApp component

---
+++ src/components/MusicApp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="music-app">
+    <TabNavigation :tabs="tabs" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TabNavigation from '@/components/TabNavigation.vue';
+
+const tabs = [
+  {
+    path: '/music/library',
+    icon: 'music',
+    label: 'Library'
+  },
+  {
+    path: '/music/player',
+    icon: 'play-circle',
+    label: 'Player'
+  }
+];
+</script>
+
+<style lang="scss">
+.music-app {
+  @include stretch-column-nowrap;
+  padding-top: $gutter;
+
+  .ui-tab-navigation-link--active {
+    > .ui-icon,
+    > .ui-tab-navigation-link-label {
+      color: $pink;
+    }
+  }
+}
+</style>
---
+++ src/views/MusicView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="iportfolio-app-view">
+    <MusicApp />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MusicApp from '@/components/MusicApp.vue';
+</script>
