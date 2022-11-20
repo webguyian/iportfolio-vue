5d2e14f78fe5a6e003280f9f8b48888eaf212654
Ian McPhail <webguyian@gmail.com>
2022-11-20 16:45:37 -0500

    Add vue3-touch-events dependency

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import Vue3TouchEvents from 'vue3-touch-events';
 import App from './App.vue';
 import router from './router';
 
@@ -6,6 +7,6 @@ import './assets/styles/global.scss';
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router).use(Vue3TouchEvents);
 
 app.mount('#app');
