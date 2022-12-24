a094251244d05b46049894a0421c0e15c3bb6afc
Ian McPhail <webguyian@gmail.com>
2022-12-24 12:04:12 -0500

    Add fromNow prop to DateTime component

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -1,22 +1,20 @@
-<template>
-  {{ datetime }}
-</template>
-
 <script setup lang="ts">
 import { computed } from 'vue';
-import { format as formatDate } from 'date-fns';
+import { format as formatDate, isToday, isYesterday } from 'date-fns';
 import { useDateTime } from '@/composables/datetime/hooks';
 
 const props = withDefaults(
   defineProps<{
     format?: string;
     fromDate?: Date;
+    fromNow?: boolean;
     initial?: number;
     relativeFormat?: string;
     stopped?: boolean;
   }>(),
   {
     format: 'h:mm',
+    fromNow: false,
     relativeFormat: 'M/d/yy',
     stopped: false
   }
@@ -25,9 +23,25 @@ const props = withDefaults(
 const emit = defineEmits(['update']);
 
 const { millis } = useDateTime(props.stopped, props.initial, emit);
+const date = new Date(millis.value);
 const datetime = computed(() =>
   props.fromDate
     ? formatDate(props.fromDate, props.format)
     : formatDate(millis.value, props.format)
 );
+const dateFromNow = computed(() =>
+  isToday(date)
+    ? formatDate(date, props.format)
+    : formatDate(date, props.relativeFormat)
+);
 </script>
+
+<template>
+  <template v-if="fromNow">
+    <template v-if="isYesterday(date)">Yesterday</template>
+    <template v-else>{{ dateFromNow }}</template>
+  </template>
+  <template v-else>
+    {{ datetime }}
+  </template>
+</template>
