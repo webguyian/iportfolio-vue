34f8d3468c70ccfec5a7c5af28cd8ba8437cd0fe
Ian McPhail <webguyian@gmail.com>
2022-12-24 11:25:46 -0500

    Create CountText component

---
+++ src/components/CountText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useSlots } from 'vue';
+import UIText from './UIText.vue';
+
+defineProps<{
+  count: number;
+  emptyLabel?: string;
+}>();
+
+const slots = useSlots();
+const [slot] = slots.default ? slots.default() : [];
+const children = String(slot && slot.children);
+const label = children.slice(0, children.length - 1);
+</script>
+
+<template>
+  <UIText v-if="count === 1" element="p"> {{ count }} {{ label }} </UIText>
+  <UIText v-else element="p">
+    {{ count || emptyLabel }}
+    <slot />
+  </UIText>
+</template>
